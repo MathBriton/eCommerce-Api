1bf16c967e6f2175d5898459d77e92745f081627
MathBriton <mathhbrito@gmail.com>
2025-02-21 11:23:33 -0300

    Subindo Extensions

--- src/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ src/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc4ee6db7d2c5cf46e297880eca9676784c15a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6b93cfb2326353c100a602620c021e345edc6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
