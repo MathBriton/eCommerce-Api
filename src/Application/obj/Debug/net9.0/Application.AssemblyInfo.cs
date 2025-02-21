0fc4ee6db7d2c5cf46e297880eca9676784c15a2
MathBriton <mathhbrito@gmail.com>
2025-02-21 11:20:04 -0300

    Subindo controllers

--- src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3dd53a57916e6068d7df1a21b924784cc868d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578d8c95c4caedc4c902ab371a30449247850597")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
